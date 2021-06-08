fe9f0a8d9aad288ba85a37bfddd1d0cabf4baa11
yufanphombolimbu <youfanlimboo@gmail.com>
2021-06-08 00:11:01 -0500

    same by author yufan limbu
    writing a simple function program in python

---
+++ function.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+# Author : Yufan Phombo Limbu
+# programming in python language
+# writing simple program in function
+
+# so what is a function ?
+'''
+A function is a block of code which can execute when we called function back again.
+
+a function in a python always start keywords with word called def 
+'''
+def fun():
+
+    print("Hello world")
+    print("Hello Nepal")
+    print("Hello yufan")
+# calling function one time
+
+fun()
