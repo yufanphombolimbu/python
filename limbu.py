31b1ec693e9c82dd6647fd34472305af5b110289
yufanphombolimbu <youfanlimboo@gmail.com>
2021-06-08 00:22:16 -0500

    by author yufan limbu
    recently changes has been done in code by author

--- limbu.py
+++ limbu.py
@@ -5,8 +5,12 @@ print("Hello yufan")
 # Author: Yufan Limbu
 # currently studying at Softwarica college of it & E-commerce
 
-a = "Yufan Limbu"
-if a == "yufan":
-    print("Variable name matches")
+a = input("Please enter your username: ")
+b = input("Please enter your password: ")
+
+if a == "yufan" and b =="limbu123":
+    print("your password matches too.")
+elif a == "yufan" and b == "limbu1234":
+    print("your password matches ")
 else:
-    print("variable name doesn't matches")
+    print("your password doesn't matches bro")
