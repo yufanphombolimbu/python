a69bef79163f3eb4a234e00f669546788e06aa4d
YufanPhomboLimbu <83822630+yufanphombolimbu@users.noreply.github.com>
2021-06-08 11:13:07 +0545

    yufan

--- limbu.py
+++ limbu.py
@@ -1,5 +1,6 @@
 #!/bin/python3
-
+import sys
+import date 
 print("Hello yufan")
 
 # Author: Yufan Limbu
@@ -7,10 +8,13 @@ print("Hello yufan")
 
 a = input("Please enter your username: ")
 b = input("Please enter your password: ")
+while 1:
+  if a == " " or len(b):
 
 if a == "yufan" and b =="limbu123":
     print("your password matches too.")
 elif a == "yufan" and b == "limbu1234":
     print("your password matches ")
+
 else:
     print("your password doesn't matches bro")
