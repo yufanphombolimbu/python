03448436a89a96e23a28ea63515f8fd42f111a7c
YoufanPhomboLimbu <youfanlimboo@gmail.com>
2021-06-07 23:20:38 -0500

    by author yufan limbu
    writing simple python if/else statement

---
+++ limbu.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+print("Hello yufan")
+
+# Author: Yufan Limbu
+# currently studying at Softwarica college of it & E-commerce
+
+a = "Yufan Limbu"
+if a == "yufan":
+    print("Variable name matches")
+else:
+    print("variable name doesn't matches")
